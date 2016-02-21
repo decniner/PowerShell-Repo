48e30391fce90d1ce5d4a5114d5a8dd9be0c2956
decniner <decniner@gmail.com>
2016-02-21 12:41:27 +0900

    Create GetFreeDiskSpace.ps1

--- GetFreeDiskSpace.ps1
+++ GetFreeDiskSpace.ps1
@@ -1,3 +1,5 @@
 "Tokwd123456", "W08R2CS01", "W08R2CS02", "W08R2SQL08", "W08R2SQL08A", "WSS08" | 
 foreach {Get-WmiObject -Class Win32_LogicalDisk `-ComputerName $_ -Filter "DeviceId='C:'" } | 
 Format-Table SystemName, @{Name="Free"; Expression={[math]::round($($_.FreeSpace/1GB), 2)}} -auto
+
+# The  free  space  is  recalculated  from  bytes  to  GB  to  make  the  results  more  understandable.
