ccdfefd51b15ad1464bc99f89dbf892018f38319
decniner <decniner@gmail.com>
2016-02-21 12:38:39 +0900

    Create GetFreeDiskSpace.ps1
    Get multiple machines free disk space info

--- GetFreeDiskSpace.ps1
+++ GetFreeDiskSpace.ps1
@@ -1,3 +1,3 @@
-"dc02", "W08R2CS01", "W08R2CS02", "W08R2SQL08", "W08R2SQL08A", "WSS08" | 
+"Tokwd123456", "W08R2CS01", "W08R2CS02", "W08R2SQL08", "W08R2SQL08A", "WSS08" | 
 foreach {Get-WmiObject -Class Win32_LogicalDisk `-ComputerName $_ -Filter "DeviceId='C:'" } | 
 Format-Table SystemName, @{Name="Free"; Expression={[math]::round($($_.FreeSpace/1GB), 2)}} -auto
