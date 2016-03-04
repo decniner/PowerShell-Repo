cdf0088d4892dbfa7d5354db680ea49dad0d17ea
decniner <decniner@gmail.com>
2016-03-04 09:18:49 +0900

    Create Exit-Function.ps1

---
+++ Exit-Function.ps1
@@ -0,0 +1,14 @@
+#Exiting a Function
+
+<#
+To exit a function immediately, use the return statement. The next function expects a name (including wildcards) and lists all matching processes. If no name is specified, the function outputs a warning message and exits the function using return:
+#>
+
+function Get-NamedProcess($name=$null) {
+    if ($name -eq $null) {
+    Write-Host -foregroundcolor Red 'Specify a name!'
+    return
+}
+
+    Get-Process $name
+}
